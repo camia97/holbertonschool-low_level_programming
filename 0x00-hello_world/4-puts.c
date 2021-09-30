45a4dfcba26d1882d64b7bbc1ef134fab32ec54e
camia97 <3962@holbertonshcool.com>
2021-09-30 07:35:30 -0700

    fix again C print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main ()
+/**
+ *main - print a string
+ *Return: 0
+ */
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
 
