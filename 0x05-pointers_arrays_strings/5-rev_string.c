c08bdc83121a6e0a279d50b18c8f5d1a4245ca87
camia97 <3962@holbertonshcool.com>
2021-10-14 07:10:12 -0700

    character of a string, starting with the first character

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: char
+ */
+void rev_string(char *s)
+{
+	int a;
+	int b = 0;
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - character of a string, starting with the first character
+ * @str: char
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0 ; str[a] != '\0' ; a++)
+	{
+		if (str[a] % 2 == 0)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
