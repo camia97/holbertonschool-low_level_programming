2b47b1d5004bfe51932ff0ac92dfd6035504d46e
camia97 <3962@holbertonshcool.com>
2021-10-24 13:35:54 -0700

    add functions to libmy

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer to string
+ * @needle: pointer substring
+ * Return: pointer to the beginning of substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int larg, lar;
+
+
+	for (larg = 0; haystack[larg]; larg++)
+	{
+	}
+		for (lar = 0; needle[lar]; lar++)
+		{
+		}
+	return (0);
+}
