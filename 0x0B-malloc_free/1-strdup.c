e3928fc3f4c599da6e6f97b18cf811f8b0185406
camia97 <3962@holbertonshcool.com>
2021-10-26 10:33:27 -0700

    pointer to duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - contains a copy of the string
+ * @str: pointer to string
+ * Return: null or pointer
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int larg, olarg;
+
+	for (larg = 0; str[larg] != '\0'; larg++)
+	{
+	}
+	ptr = malloc(sizeof(char) * larg);
+		if (ptr == 0)
+			return (NULL);
+	for (olarg = 0; ptr[olarg] <= larg; olarg++)
+	{
+		ptr[olarg] = str[olarg];
+	}
+	return (ptr);
+
+}
