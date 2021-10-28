f65f2d3975ccb8ae29c131f5efe049fac9e0058c
camia97 <3962@holbertonshcool.com>
2021-10-28 12:46:51 -0700

    function that allocates memory for an array

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,5 +35,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	ptr[larg4] = '\0';
 	return (ptr);
-	return (NULL);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory of an array
+ * @nmemb: number of mem
+ * @size: size of each mem
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int larg;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	ptr = malloc(size * nmemb);
+	if (ptr == 0)
+		return (NULL);
+	for (larg = 0; larg < nmemb; larg++)
+		ptr[larg] = 0;
+	return ((void *)ptr);
+}
