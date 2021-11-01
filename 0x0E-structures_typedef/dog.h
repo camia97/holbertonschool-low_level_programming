0b178e183fce30f642c535db0ae6ccf7b354a03f
camia97 <3962@holbertonshcool.com>
2021-11-01 12:36:29 -0700

    define new structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new structure
+ * @name: type char
+ * @age: type float
+ * @owner: type char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
