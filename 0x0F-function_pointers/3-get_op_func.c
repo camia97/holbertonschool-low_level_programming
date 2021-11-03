d254c51454d7409372328bc1a7c8e834c8f794b9
camia97 <3962@holbertonschool.com>
2021-11-03 16:28:50 -0700

    fix all

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,15 +8,14 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = 
-{
+	op_t ops[] = {
 	{"+", op_add},
 	{"-", op_sub},
 	{"*", op_mul},
 	{"/", op_div},
 	{"%", op_mod},
 	{NULL, NULL}
-};
+	};
 	int a = 0;
 
 	while (a < 5)
