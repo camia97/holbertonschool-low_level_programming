f1dd549062153dc2e5340b8348dd7c7d8cdd9cc0
camia97 <3962@holbertonschool.com>
2021-11-03 10:49:21 -0700

    executes a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a given function
+ * @array: pointer to array
+ * @size: size of array
+ * @action: pointer to the function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a;
+
+	for (a = 0; a <= size; a++)
+		action(array[a]);
+}
