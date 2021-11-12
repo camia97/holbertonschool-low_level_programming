82ae5850e4154cacc8f79845d95a315fdf90eded
camia97 <3962@holbertonschool.com>
2021-11-11 16:19:09 -0800

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * cleanupfunc - function that prints before main
+ * Return: void
+ */
+void cleanupfunc(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
