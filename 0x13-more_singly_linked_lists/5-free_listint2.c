b3bcbf7599edfc389a5f25fa535dcc2d54a173d1
camia97 <3962@holbertonschool.com>
2021-11-16 09:41:55 -0800

    free the listint_t head null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free listint_t
+ * @head: pointer to the node of the struct
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	while (*head)
+	{
+		aux = *head;
+		(*head) = (*head)->next;
+		free(aux);
+	}
+	head = NULL;
+}
