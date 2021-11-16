4342447651e61db2f81f0f88b91e76ab8b77a77d
camia97 <3962@holbertonschool.com>
2021-11-16 13:48:37 -0800

    add idx - 1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			*head = new;
 			return (*head);
 		}
-		while (pos < idx)
+		while (pos < idx - 1)
 		{
 			if (tmp->next)
 				tmp = tmp->next;
