9758303df8180c22f2df5c76a9668007ee4ff463
camia97 <3962@holbertonschool.com>
2021-11-22 09:41:02 -0800

    change write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	a = read(aux, buffer, letters);
 	if (a == -1)
 		return (0);
-	c = write(1, buffer, a);
+	c = write(STDIN_FILENO, buffer, a);
 	if (c == -1)
 	{
 		free(buffer);
