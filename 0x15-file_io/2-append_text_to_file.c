2e7d4abd69fbee4059e5674d67836783fb9d1ed1
camia97 <3962@holbertonschool.com>
2021-11-22 11:04:59 -0800

    appends a text to the end

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end
+ * @filename: name of the file
+ * @text_content: text content
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int aux, c, w;
+
+	if (!filename)
+		return (-1);
+
+	aux = open(filename, O_WRONLY | O_APPEND);
+
+	if (!aux)
+		return (-1);
+	for (c = 0; text_content[c]; c++)
+		;
+	w = write(aux, text_content, c);
+
+	if (w == -1)
+	{
+		close(aux);
+		return (-1);
+	}
+	return (1);
+}
