b5d1856c26b1b6df7b6df3e0fec2c4c28c7b0911
camia97 <3962@holbertonschool.com>
2021-11-22 10:35:14 -0800

    add close

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (aux == -1)
 		return (-1);
+	if (!text_content)
+	{
+		close(aux);
+		return (1);
+	}
 	for (c = 0; text_content[c]; c++)
 		;
 	w = write(aux, text_content, c);
