5eeb2d66dced08d005febb8e23b6dae1c8704851
camia97 <3962@holbertonschool.com>
2022-01-28 11:47:32 -0800

    creates hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash;
 	unsigned long int i = 0;
 
-	new_hash = malloc(sizeof(hash_table_t) * size);
+	new_hash = malloc(sizeof(hash_table_t));
 	if (!new_hash)
 		return (NULL);
 	new_hash->size = size;
