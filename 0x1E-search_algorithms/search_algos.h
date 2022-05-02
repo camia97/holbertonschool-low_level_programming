3d5dcf2e552509db0f6d28f4c63e5db474769199
camia97 <3962@holbertonschool.com>
2022-05-02 15:05:02 +0000

    Search Algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+#include <stdio.h>
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
